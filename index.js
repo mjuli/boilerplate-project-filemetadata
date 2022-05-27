d85dde6d3df708b424c15873840342ad948934e0
Julia Martins <mjuh.ms@gmail.com>
2022-05-26 21:03:43 -0300

    Adding `/api/fileanalyse` post route

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
 require('dotenv').config()
 
-var app = express();
+const app = express();
+
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const fileData = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
 
-
+  res.json(fileData)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
